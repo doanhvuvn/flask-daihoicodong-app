9fb829afbad6fec4cc0c3828d524723969b2ef50
doanhvuvn <doanhvuvn@gmail.com>
2025-08-22 20:07:14 +0700

    Delete temp.py

--- temp.py
+++
@@ -1,9 +0,0 @@
-column_mapping = {
-    'macd': 'ma_co_dong',
-    'hoten': 'ho_ten',
-    'sodksh': 'cmnd',
-              'ngaycap': 'ngay_cap',
-              'socophan': 'so_luong_cp'
-}
-# Dùng hàm rename để đổi tên các cột trong DataFrame
-df.rename(columns=column_mapping, inplace=True)
